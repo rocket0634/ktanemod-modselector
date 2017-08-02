b11d74b2b843ddf3b3cf6da56a046a8a37460a42
Ashley Muncaster <ashley@atbweb.co.uk>
2017-08-02 20:28:06 +0100

    Mod info page will now list the module IDs of all modules within the specific mod asset bundle.

--- Assets/Scripts/ModWrapperInfoPage.cs
+++ Assets/Scripts/ModWrapperInfoPage.cs
@@ -13,7 +13,7 @@ public class ModWrapperInfoPage : MonoBehaviour
 
     private TabletPage _tabletPage = null;
 
-    private const string FormattingString = "<b>Title</b>:\t{0}\n<b>ID</b>:\t{1}\n<b>Version</b>:\t{2}\n<b>Path</b>:\t<size={3}>{4}</size>\n\n<b>Included Mod Objects</b>:\n{5}";
+    private const string FormattingString = "<b>Title</b>:\t{0}\n<b>ID</b>:\t{1}\n<b>Version</b>:\t{2}\n<b>Path</b>:\t<size={3}>{4}</size>\n<b>Mod Object Prefabs</b>:\n{5}\n<b>Mod Module IDs</b>:\n{6}";
 
     private void Awake()
     {
@@ -34,36 +34,78 @@ public class ModWrapperInfoPage : MonoBehaviour
 
         if (modWrapper == null)
         {
-            detailText.text = string.Format(FormattingString, "**NULL**", "**NULL**", "**NULL**", 24, "**NULL**", "**NULL**");
+            detailText.text = string.Format(FormattingString, "**NULL**", "**NULL**", "**NULL**", 24, "**NULL**", "**NULL**", "**NULL**");
             return;
         }
 
         int pathFontSize = Mathf.Clamp((maximumSize * pathCharacterCutOff) / modWrapper.ModDirectory.Length, minimumSize, maximumSize);
 
         StringBuilder modStringBuilder = new StringBuilder();
-        StringBuilder lineStringBuilder = new StringBuilder();
+        StringBuilder modLineStringBuilder = new StringBuilder();
+
+        StringBuilder moduleIDStringBuilder = new StringBuilder();
+        StringBuilder moduleIDLineStringBuilder = new StringBuilder();
         foreach(GameObject gameObject in modWrapper.ModObjects)
         {
-            if (lineStringBuilder.Length > 0)
+            if (modLineStringBuilder.Length > 0)
             {
-                lineStringBuilder.Append(", ");
+                modLineStringBuilder.Append(", ");
             }
 
             string addition = gameObject.name.Replace("(Clone)", "");
-            if (lineStringBuilder.Length + addition.Length > modCharacterCutOff)
+            if (modLineStringBuilder.Length + addition.Length > modCharacterCutOff)
             {
-                modStringBuilder.Append(lineStringBuilder.ToString());
+                modStringBuilder.Append(modLineStringBuilder.ToString());
                 modStringBuilder.Append("\n");
-                lineStringBuilder = new StringBuilder();
+                modLineStringBuilder = new StringBuilder();
             }
 
-            lineStringBuilder.Append(addition);
+            modLineStringBuilder.Append(addition);
+
+            KMBombModule[] regularModules = gameObject.GetComponentsInChildren<KMBombModule>(true);
+            foreach(KMBombModule regularModule in regularModules)
+            {
+                if (moduleIDLineStringBuilder.Length > 0)
+                {
+                    moduleIDLineStringBuilder.Append(", ");
+                }
+
+                if (moduleIDLineStringBuilder.Length + regularModule.ModuleType.Length > modCharacterCutOff)
+                {
+                    moduleIDStringBuilder.Append(moduleIDLineStringBuilder.ToString());
+                    moduleIDStringBuilder.Append("\n");
+                    moduleIDLineStringBuilder = new StringBuilder();
+                }
+
+                moduleIDLineStringBuilder.Append(regularModule.ModuleType);
+            }
+
+            KMNeedyModule[] needyModules = gameObject.GetComponentsInChildren<KMNeedyModule>(true);
+            foreach(KMNeedyModule needyModule in needyModules)
+            {
+                if (moduleIDLineStringBuilder.Length > 0)
+                {
+                    moduleIDLineStringBuilder.Append(", ");
+                }
+
+                if (moduleIDLineStringBuilder.Length + needyModule.ModuleType.Length > modCharacterCutOff)
+                {
+                    moduleIDStringBuilder.Append(moduleIDLineStringBuilder.ToString());
+                    moduleIDStringBuilder.Append("\n");
+                    moduleIDLineStringBuilder = new StringBuilder();
+                }
+
+                moduleIDLineStringBuilder.Append(needyModule.ModuleType);
+            }
         }
 
-        modStringBuilder.Append(lineStringBuilder);
-        string modStringText = modStringBuilder.ToString();
+        modStringBuilder.Append(modLineStringBuilder);
+        moduleIDStringBuilder.Append(moduleIDLineStringBuilder);
 
-        detailText.text = string.Format(FormattingString, modWrapper.ModTitle, modWrapper.ModName, modWrapper.ModVersion, pathFontSize, modWrapper.ModDirectory, modStringText);
+        string modStringText = modStringBuilder.ToString();
+        string moduleIDStringText = moduleIDStringBuilder.ToString();
+
+        detailText.text = string.Format(FormattingString, modWrapper.ModTitle, modWrapper.ModName, modWrapper.ModVersion, pathFontSize, modWrapper.ModDirectory, modStringText, moduleIDStringText);
     }   
     
     public void OpenPath()
