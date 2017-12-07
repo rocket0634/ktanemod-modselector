57d0d8e7c58a743f1623953e999d032e8f21e451
Ashley Muncaster <ashley@atbweb.co.uk>
2017-12-07 22:07:38 +0000

    Fix for badly loaded mods

--- Assets/Scripts/Helpers/ReflectionHelper.cs
+++ Assets/Scripts/Helpers/ReflectionHelper.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 public static class ReflectionHelper
 {
     public static Type FindType(string fullName)
     {
-        return AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.FullName.Equals(fullName));
+        return AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetSafeTypes()).FirstOrDefault(t => t.FullName.Equals(fullName));
+    }
+
+    private static IEnumerable<Type> GetSafeTypes(this Assembly assembly)
+    {
+        try
+        {
+            return assembly.GetTypes();
+        }
+        catch (ReflectionTypeLoadException e)
+        {
+            return e.Types.Where(x => x != null);
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
     }
 }
