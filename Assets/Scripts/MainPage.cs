f89810656cdcf3b03403d8935cda24fc3495556e
Ashley Muncaster <ashley@atbweb.co.uk>
2018-04-25 21:52:49 +0100

    Add reload profiles button.
    Fixes #41.

--- Assets/Scripts/MainPage.cs
+++ Assets/Scripts/MainPage.cs
@@ -3,6 +3,11 @@
 [RequireComponent(typeof(TabletPage))]
 public class MainPage : MonoBehaviour
 {
+    public void ReloadProfiles()
+    {
+        Profile.ReloadActiveConfiguration();
+    }
+
     public void OpenModProfilesFolder()
     {
         Application.OpenURL(string.Format("file://{0}", Profile.ProfileDirectory));
